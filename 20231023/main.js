6ccc224f73af419780849c5d3f6541dc66ee4396
unknown <hasinofi@outlook.com>
2023-10-24 13:40:14 +0900

    JS add my name

--- 20231023/main.js
+++ 20231023/main.js
@@ -1,1 +1,13 @@
 console.log("Hi");
+
+let h2 = document.createElement("h2");
+// <h2></h2>
+
+// 접근 명령어 getElementById
+let dom = document.getElementById("dom");
+
+// 텍스트를 집어 넣는다.
+h2.innerText = "My name is Junhyuk Jung"
+
+// dom을 h2의 자식으로 붙힌다.
+dom.appendChild(h2);
